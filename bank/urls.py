34340c583513957cca307b79dc82e16f94b20906
Saifullakh1 <saifullakh7@gmail.com>
2026-03-16 14:34:12 +0600

    added simple jwt

--- bank/urls.py
+++ bank/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 api_urlpatterns = [
     path('clients/', include('clients.urls')),
@@ -26,9 +29,12 @@ api_urlpatterns = [
     path('transfer/', include('transfers.urls'))
 ]
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(api_urlpatterns))
+    path('api/', include(api_urlpatterns)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- clients/views.py
+++ clients/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
 from .models import Client
 from .serializers import ClientSerializer
+from rest_framework import permissions
 
 
 class ClientListAPIView(generics.ListCreateAPIView):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
 
 
 class ClientRetrieveAPIView(generics.RetrieveUpdateDestroyAPIView):
