a53f1434699b21f291192cff46ee155d56dcc876
Saifullakh1 <saifullakh7@gmail.com>
2026-04-06 14:12:16 +0600

    integrated supabase

--- bank/settings.py
+++ bank/settings.py
@@ -80,12 +80,26 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.prhcbenqwjhfhfmwcwly',
+        'PASSWORD': 'zef-ywty-vkc',
+        'HOST': 'aws-1-ap-northeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
